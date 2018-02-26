d15f6012f1372b86c0255f7fb2e77fee5a93fe0a
Rainer Keller <mail@rainerkeller.de>
2018-02-26 18:00:15 +0100

    Add JSON schema for event protocol

---
+++ clubcockpit/test-schema.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.5
+
+import jsonschema
+import json
+
+with open("schema.json") as f:
+    schema = json.loads(f.read())
+
+obj = json.loads("{\"version\": 1, \"type\":\"S\",\"title\":\"fefefaefe\",\"location\":{\"name\":\"afefafaefe\",\"address\":\"afsdfs\",\"postcode\":\"asdfsd\",\"city\":\"asdfsadf\",\"country\":\"AT\"},\"dates\":[[\"2018-02-24\",\"2018-02-27\"]],\"leader\": [\"leader1\", \"leader2\"], \"special\":{\"halls\":[1,1,1,1]},\"gemaBackingOtherwise\":false,\"contact\":{\"person\":\"name\",\"email\":\"email\",\"phone\":\"phone\"},\"publish\":{\"url\":\"http://foo\",\"calendar\": true}, \"dancelevels\": [\"MS\", \"PL\"]}")
+print(obj)
+
+jsonschema.validate(obj, schema)
